5db3c38d50d1554dd1cfda9ef603c33d45b82ff5
Christopher Beeson <chrisbeeson@me.com>
2020-04-17 20:04:51 +1000

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDk2D8-WxmtSM3RvWeFm99UnhqdQvFftsM",
+    authDomain: "worldpump-pandemic.firebaseapp.com",
+    databaseURL: "https://worldpump-pandemic.firebaseio.com",
+    projectId: "worldpump-pandemic",
+    storageBucket: "worldpump-pandemic.appspot.com",
+    messagingSenderId: "703715173552",
+    appId: "1:703715173552:web:3ab405efba26a75fc7c775",
+    measurementId: "G-513C8F9W83"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.

81e8b666c9c4b35cff78d43942755dca58cfaacc
Christopher Beeson <chrisbeeson@me.com>
2020-06-27 11:43:38 +1000

    Added Insomnia to prevent screen sleeping

--- src/app/pages/rundown/services/attendance.service.ts
+++ src/app/pages/rundown/services/attendance.service.ts
@@ -6,6 +6,10 @@ import { Workout } from '../../../models/interfaces';
 import * as firebase from 'firebase/app';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { firestore } from 'firebase';
+import { Plugins, AppState } from '@capacitor/core';
+const { App } = Plugins;
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +19,13 @@ export class AttendanceService {
   public allowed_to_attend = true;  // if user quits during a pump they can no longer attend the workout.
   public currently_attending = false;
   private _currentWorkout: Workout;
+  private _currentStep: number;
 
   constructor(
     private rundownService: RundownService,
     private profileService: ProfileService,
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    private insomnia: Insomnia
   ) {
 
     this.rundownService.currentWorkout$.pipe(distinct()).subscribe(workout => {
@@ -33,6 +39,12 @@ export class AttendanceService {
       if (workout && this.allowed_to_attend) {
         this.setCurrentlyAttending(true, workout.uid);
         this._currentWorkout = workout;
+        this.insomnia.keepAwake()
+          .then(
+            () => console.log('[insomnia] keepAwake() success'),
+            (err) => console.log('[insomnia] error: ' + err)
+          );
+
         return;
       }
 
@@ -41,7 +53,7 @@ export class AttendanceService {
         this.completedWorkout(workout.uid);
         this._currentWorkout = null;
       }
-*/
+      */
 
     });
 
@@ -52,11 +64,24 @@ export class AttendanceService {
         this.currently_attending = true;
       }
 
-      if (step.name == 'workout_complete') {
+      if (step.name == 'workout_complete' && this.allowed_to_attend) {
         this.completedWorkout();    // The only way to publish is on this step.
       }
     });
     // watch for plaform move to background after step 4
+
+    App.addListener('appStateChange', (state: AppState) => {
+      // state.isActive contains the active state
+      console.log('App state changed. Is active?', state.isActive);
+
+      if (!state.isActive) {
+        if (this._currentStep > 4)
+          this.allowed_to_attend = false;
+        this.setCurrentlyAttending(false, this._currentWorkout.uid)
+      }
+
+    });
+
   }
 
 
@@ -94,6 +119,13 @@ export class AttendanceService {
     }));
 
     await batch.commit();
+
+    this.insomnia.allowSleepAgain()
+      .then(
+        () => console.log('[Insomina] allowSleepAgain() success'),
+        (err) => console.log('[Insomina] error: ' + err)
+      );
+
   }
 
 
@@ -106,6 +138,11 @@ export class AttendanceService {
       // if (this.profileService.profileDoc.workout_currently_attending){
       await this.profileService.profileDoc.update({ workout_currently_attending: firebase.firestore.FieldValue.delete() });
       // }
+      this.insomnia.allowSleepAgain()
+      .then(
+        () => console.log('[Insomina] allowSleepAgain() success'),
+        (err) => console.log('[Insomina] error: ' + err)
+      );
     }
   }
 
